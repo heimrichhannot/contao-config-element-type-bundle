3ba6ebbf67fde2839d47e4b3fc507c46bc8ea5af
Thomas Körner <t.koerner@heimrich-hannot.de>
2020-08-17 15:40:42 +0200

    fixed resources folder name, added translations for config type elements

---
+++ src/Resources/contao/languages/de/tl_list_config_element.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Copyright (c) 2020 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+$lang = &$GLOBALS['TL_LANG']['tl_list_config_element'];
+
+$lang['reference'][\HeimrichHannot\ConfigElementTypeBundle\ConfigElementType\Concrete\ImageGalleryConfigElementType::getType()] = 'Bildergallerie';
---
+++ src/Resources/contao/languages/de/tl_reader_config_element.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Copyright (c) 2020 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+$GLOBALS['TL_LANG']['tl_reader_config_element']['reference'][\HeimrichHannot\ConfigElementTypeBundle\ConfigElementType\Concrete\ImageGalleryConfigElementType::getType()] = 'Bildergallerie';
+
+$lang = &$GLOBALS['TL_LANG']['tl_reader_config_element'];
+
+$lang['reference'][\HeimrichHannot\ConfigElementTypeBundle\ConfigElementType\Concrete\ImageGalleryConfigElementType::getType()] = 'Bildergallerie';
